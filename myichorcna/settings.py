30759ad7045756f42e9c7b6793d295e8e743e27f
Matteo Lepur <lepurmatteo@gmail.com>
2024-08-29 09:52:32 -0700

    create a directory for different settings ill run ichorCNA at

--- myichorcna/settings.py
+++ myichorcna/settings.py
@@ -8,9 +8,9 @@ class RelativePaths:
     repo: str = "ichorCNA"
     rscript: str = "ichorCNA/scripts/runIchorCNA.R"
 
-    gc_wig: str = "ichorCNA/inst/extdata/gc_hg19_500kb.wig"
-    map_wig: str = "ichorCNA/inst/extdata/map_hg19_500kb.wig"
-    normal_panel: str = "ichorCNA/inst/extdata/HD_ULP_PoN_500kb_median_normAutosome_mapScoreFiltered_median.rds"
+    gc_wig: str = "ichorCNA/inst/extdata/gc_hg19_1000kb.wig"
+    map_wig: str = "ichorCNA/inst/extdata/map_hg19_1000kb.wig"
+    normal_panel: str = "ichorCNA/inst/extdata/HD_ULP_PoN_1Mb_median_normAutosome_mapScoreFiltered_median.rds"
     centromere: str = "ichorCNA/inst/extdata/GRCh37.p13_centromere_UCSC-gapTable.txt"
     genome_build: str = "hg19"  # not included in hg38
     genome_style: str = "NCBI"
