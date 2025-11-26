79d445f119f2e974dbd816926ce4a60e76978695
Matteo Lepur <lepurmatteo@gmail.com>
2025-11-25 16:40:45 -0800

    create function to only call hmmsegment

--- myichorcna/cli.py
+++ myichorcna/cli.py
@@ -1,5 +1,5 @@
 import click
-from myichorcna.call_ichorcna import run_ichorCNA
+from myichorcna.call_ichorcna import run_ichorCNA, run_hmmsegment
 
 @click.command()
 @click.option(
@@ -33,6 +33,40 @@ from myichorcna.call_ichorcna import run_ichorCNA
 )
 def run(**kwargs):
     run_ichorCNA(**kwargs)
+    
+    
+@click.command()
+@click.option(
+    '-o',
+    '--output-dir',
+    type=click.STRING,
+    required=True,
+    help="Path to directory to output results from ichorCNA."
+)
+@click.option(
+    '-c',
+    '--ctdna-tsv-file',
+    type=click.STRING,
+    required=True,
+    help="Path to read counts of ctDNA data outputted by readCounter (.wig)"
+)
+@click.option(
+    "-d",
+    '--sample-id',
+    default='DummySampleID',
+    type=click.STRING,
+    required=False,
+    help="cfDNA sample ID"
+)
+@click.option(
+    '-s',
+    '--ichorcna-params',
+    type=click.STRING,
+    required=True,
+    help="Path to config file for settings of ichorCNA (.yaml)."
+)
+def run_hmm(**kwargs):
+    run_hmmsegment(**kwargs)
 
 
 @click.group(name='myichorcna')
@@ -41,6 +75,7 @@ def main():
 
 
 main.add_command(run)
+main.add_command(run_hmm)
 
 
 if __name__ == "__main__":
